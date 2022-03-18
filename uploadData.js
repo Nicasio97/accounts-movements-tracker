cef010f6420928c7a9d50a18f06ff0dea6ec3488
nicasio-hp_chica <nicasiomontes97@gmail.com>
2022-03-18 16:27:23 -0300

    added code to extract specific fields from bank movements

--- uploadData.js
+++ uploadData.js
@@ -8,21 +8,21 @@ brou.getExchangeToken(c.brou.user,c.brou.password).then((exchangeToken) => {
             brou.getMovementsFromAccount(object.bearerToken,object.accountId,"2022-02-01","2022-02-25").then((value)=>{                
                 let arrayMovements = value.data.statements;
                 let dataRowsArray = [];                    
-                console.log(arrayMovements);
-                // for (let i = 0; i < arrayMovements.length; i++) {                    
-                //     let date = arrayMovements[i].date.split("T")[0];
-                //     let hour = arrayMovements[i].date.split("T")[1].split(".")[0];
-                //     let concept = arrayMovements[i].concept
-                //     let reference = arrayMovements[i].reference
-                //     let debit = arrayMovements[i].debit
-                //     let credit = arrayMovements[i].credit
-                //     let documentNumber = arrayMovements[i].documentNumber
+                //console.log(arrayMovements);
+                for (let i = 0; i < arrayMovements.length; i++) {                    
+                    let date = arrayMovements[i].date.split("T")[0];
+                    let hour = arrayMovements[i].date.split("T")[1].split(".")[0];
+                    let concept = arrayMovements[i].concept
+                    let reference = arrayMovements[i].reference
+                    let debit = arrayMovements[i].debit
+                    let credit = arrayMovements[i].credit
+                    let documentNumber = arrayMovements[i].documentNumber
                     
-                //     let row = [date,hour,concept,reference,debit,credit,documentNumber];
-                //     dataRowsArray.push(row);
-                // }
-                // //apiGoogleSheet.pushData("ebrou_uyu",dataRowsArray);
-                // console.log(dataRowsArray);
+                    let row = [date,hour,concept,reference,debit,credit,documentNumber];
+                    dataRowsArray.push(row);
+                }
+                //apiGoogleSheet.pushData("ebrou_uyu",dataRowsArray);
+                console.log(dataRowsArray);
             },(err)=>{
                 throw new Error(err);
             });
