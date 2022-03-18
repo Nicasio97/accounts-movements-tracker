1ebf05ce1011d9e210a350e6b4f526bd9c647989
nicasio-hp_chica <nicasiomontes97@gmail.com>
2022-03-18 16:44:52 -0300

    create a function for the code thats extract specific fields from brou movements

--- uploadData.js
+++ uploadData.js
@@ -6,22 +6,23 @@ brou.getExchangeToken(c.brou.user,c.brou.password).then((exchangeToken) => {
     brou.getBearerToken(exchangeToken).then((bearerToken)=>{                
         brou.getAccountId(bearerToken,c.brou.accountsNumbers.usd).then((object)=>{            
             brou.getMovementsFromAccount(object.bearerToken,object.accountId,"2022-02-01","2022-02-25").then((value)=>{                
-                let arrayMovements = value.data.statements;
-                let dataRowsArray = [];                    
-                //console.log(arrayMovements);
-                for (let i = 0; i < arrayMovements.length; i++) {                    
-                    let date = arrayMovements[i].date.split("T")[0];
-                    let hour = arrayMovements[i].date.split("T")[1].split(".")[0];
-                    let concept = arrayMovements[i].concept
-                    let reference = arrayMovements[i].reference
-                    let debit = arrayMovements[i].debit
-                    let credit = arrayMovements[i].credit
-                    let documentNumber = arrayMovements[i].documentNumber
+                //let arrayMovements = value.data.statements;
+                // let dataRowsArray = [];                    
+                // //console.log(arrayMovements);
+                // for (let i = 0; i < arrayMovements.length; i++) {                    
+                //     let date = arrayMovements[i].date.split("T")[0];
+                //     let hour = arrayMovements[i].date.split("T")[1].split(".")[0];
+                //     let concept = arrayMovements[i].concept
+                //     let reference = arrayMovements[i].reference
+                //     let debit = arrayMovements[i].debit
+                //     let credit = arrayMovements[i].credit
+                //     let documentNumber = arrayMovements[i].documentNumber
                     
-                    let row = [date,hour,concept,reference,debit,credit,documentNumber];
-                    dataRowsArray.push(row);
-                }
+                //     let row = [date,hour,concept,reference,debit,credit,documentNumber];
+                //     dataRowsArray.push(row);
+                // }
                 //apiGoogleSheet.pushData("ebrou_uyu",dataRowsArray);
+                let dataRowsArray = extractFields(value.data.statements);
                 console.log(dataRowsArray);
             },(err)=>{
                 throw new Error(err);
@@ -36,3 +37,21 @@ brou.getExchangeToken(c.brou.user,c.brou.password).then((exchangeToken) => {
     throw new Error(err);
 });
 
+//Arrow vs Normal functions: https://dmitripavlutin.com/differences-between-arrow-and-regular-functions/#1-this-value 
+
+function extractFields(arrayMovements) {
+    let dataRowsArray = [];                    
+    for (let i = 0; i < arrayMovements.length; i++) {                    
+        let date = arrayMovements[i].date.split("T")[0];
+        let hour = arrayMovements[i].date.split("T")[1].split(".")[0];
+        let concept = arrayMovements[i].concept
+        let reference = arrayMovements[i].reference
+        let debit = arrayMovements[i].debit
+        let credit = arrayMovements[i].credit
+        let documentNumber = arrayMovements[i].documentNumber
+        
+        let row = [date,hour,concept,reference,debit,credit,documentNumber];
+        dataRowsArray.push(row);
+    };      
+    return dataRowsArray
+}
