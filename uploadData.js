cfa753f22f0672e26f22a1e654773354cbad2fbf
nicasio-hp_chica <nicasiomontes97@gmail.com>
2022-03-18 16:52:50 -0300

    added code to extract data from uyu account (same block code ass usd account)

--- uploadData.js
+++ uploadData.js
@@ -4,32 +4,31 @@ const brou = require("./api_brou");
 
 brou.getExchangeToken(c.brou.user,c.brou.password).then((exchangeToken) => {    
     brou.getBearerToken(exchangeToken).then((bearerToken)=>{                
-        brou.getAccountId(bearerToken,c.brou.accountsNumbers.usd).then((object)=>{            
-            brou.getMovementsFromAccount(object.bearerToken,object.accountId,"2022-02-01","2022-02-25").then((value)=>{                
-                //let arrayMovements = value.data.statements;
-                // let dataRowsArray = [];                    
-                // //console.log(arrayMovements);
-                // for (let i = 0; i < arrayMovements.length; i++) {                    
-                //     let date = arrayMovements[i].date.split("T")[0];
-                //     let hour = arrayMovements[i].date.split("T")[1].split(".")[0];
-                //     let concept = arrayMovements[i].concept
-                //     let reference = arrayMovements[i].reference
-                //     let debit = arrayMovements[i].debit
-                //     let credit = arrayMovements[i].credit
-                //     let documentNumber = arrayMovements[i].documentNumber
-                    
-                //     let row = [date,hour,concept,reference,debit,credit,documentNumber];
-                //     dataRowsArray.push(row);
-                // }
-                //apiGoogleSheet.pushData("ebrou_uyu",dataRowsArray);
+        
+        brou.getAccountId(bearerToken,c.brou.accountsNumbers.uyu).then((object)=>{            
+            brou.getMovementsFromAccount(object.bearerToken,object.accountId,"2022-02-01","2022-02-25").then((value)=>{                                        
                 let dataRowsArray = extractFields(value.data.statements);
                 console.log(dataRowsArray);
+                //apiGoogleSheet.pushData("ebrou_usd",dataRowsArray);
             },(err)=>{
                 throw new Error(err);
             });
         },(err)=>{
             throw new Error(err);
         });
+
+        brou.getAccountId(bearerToken,c.brou.accountsNumbers.usd).then((object)=>{            
+            brou.getMovementsFromAccount(object.bearerToken,object.accountId,"2022-02-01","2022-02-25").then((value)=>{                                        
+                let dataRowsArray = extractFields(value.data.statements);
+                console.log(dataRowsArray);
+                //apiGoogleSheet.pushData("ebrou_usd",dataRowsArray);
+            },(err)=>{
+                throw new Error(err);
+            });
+        },(err)=>{
+            throw new Error(err);
+        });
+
     },(err)=>{
         throw new Error(err);
     });
